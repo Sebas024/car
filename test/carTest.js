09ad88075c3904c3a7fe4236e7c9bd401f31de92
Charles Samonte <charles.deo024@hotmail.ca>
2023-06-03 17:42:52 -0600

    update

--- test/carTest.js
+++ test/carTest.js
@@ -60,7 +60,7 @@ describe("#shouldBuyCar()", function() {
 
   it("should return true when the car has 6 litres/100km and is under or equal to $5,000", function() {
     const car = {
-      litres100km: "6 litres/100km", underequal: "$5000"
+      litresPer100km: 6, price: 5000
     };
 
     const shouldBuy = shouldBuyCar(car);
@@ -68,6 +68,55 @@ describe("#shouldBuyCar()", function() {
   });
 
 });
+
+
+
+it("should return true if it's 11 litres/100km  and under or equal to $5,000", function() {
+  const car = {
+    litresPer100km: 11,
+    price: 5000
+  };
+  const shouldBuy = shouldBuyCar(car);
+  assert.isTrue(shouldBuy);
+});
+
+it("should return true if it's 6 litres/100km and over $5,000", function() {
+  const car = {
+    litresPer100km: 6,
+    price: 5001
+  };
+  const shouldBuy = shouldBuyCar(car);
+  assert.isFalse(shouldBuy);
+});
+
+it("should return true if it's 11 litres/100km and over $5,000", function() {
+  const car = {
+    litresPer100km: 11,
+    price: 5001
+  };
+  const shouldBuy = shouldBuyCar(car);
+  assert.isFalse(shouldBuy);
+});
+
+
+it("should return false if it's 5 litres/100km and under or equal to $5,000", function() {
+  const car = {
+    litresPer100km: 5,
+    price: 5000
+  };
+  const shouldBuy = shouldBuyCar(car);
+  assert.isFalse(shouldBuy);
+});
+  
+it("should return true if it's 12 litres/100km and under or equal to $5,000", function() {
+  const car = {
+    litresPer100km: 12,
+    price: 5000
+  };
+  const shouldBuy = shouldBuyCar(car);
+  assert.isFalse(shouldBuy);
+});
+  
 
 
 
